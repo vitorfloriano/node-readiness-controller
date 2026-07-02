1182cd4d09d6ed180c0f362926863eb3497f06a2
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2026-07-01 23:08:10 -0300

    add dummy spec so gingko doesn't skip.

---
+++ test/scale/scale_test.go
@@ -0,0 +1,12 @@
+package scale_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test Test Test", func() {
+	It("tests is ginkgo is initializing", func() {
+		Expect(true).To(BeTrue())
+	})
+})
