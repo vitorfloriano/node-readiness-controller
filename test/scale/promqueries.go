aef6528eac507f9e0def0c75a09c8b079982f0e1
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2026-07-16 10:37:26 -0300

    rename package to scale

--- test/scale/promqueries.go
+++ test/scale/promqueries.go
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package scale_test
+package scale
 
 // MetricQuery defines a query format for scraping Prometheus during scale tests.
 type MetricQuery struct {
